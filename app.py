07c4971e7934d6fc4548cf830334e1866e66cead
Austin Fell <austinrf2918@yahoo.com>
2018-03-11 19:56:17 -0400

    MMAPSAPI-1 Got domain objects configured with flask restful

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def root():
-    return "I am root."
+class Pin(Resource):
+    def get(self):
+        return {"lol": "haha"}
 
-app.run()
+class DecalImage(Resource):
+    def get(self):
+        return {"url": "blah"}
+
+api.add_resource(Pin, "/pins")
+api.add_resource(DecalImage, "/decals")
+
+app.run(debug = True)
