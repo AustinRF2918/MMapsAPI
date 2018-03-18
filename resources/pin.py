0a24742aca161180c83ec6e3c028e2d5c943b518
Austin Fell <austinrf2918@yahoo.com>
2018-03-18 17:38:08 -0400

    oops

--- resources/pin.py
+++ resources/pin.py
@@ -1,26 +1,27 @@
 """Defines Pin resource."""
 from flask import request
-from flask_restful import Resource, marshal_with_field
+from flask_restful import Resource
 
 from schema.pin import pin_request_schema, pin_pointer_schema, pin_response_schema
 from util.dao import MongoDao
+from util.schema_tools import marshal_with_schema
 
 pin_dao = MongoDao(pin_request_schema, "localhost", 27017)
 
 
 class PinList(Resource):
-    @marshal_with_field(pin_response_schema)
+    @marshal_with_schema(pin_response_schema)
     def get(self):
         return pin_dao.get_all()
 
-    @marshal_with_field(pin_response_schema)
+    @marshal_with_schema(pin_response_schema)
     def post(self):
         pin = request.get_json(force=True)
         return pin_dao.add_item(pin)
 
 
 class PinRevisionList(Resource):
-    @marshal_with_field(pin_pointer_schema)
+    @marshal_with_schema(pin_pointer_schema)
     def get(self):
         return pin_dao.get_all()
 
@@ -28,15 +29,15 @@ class PinRevisionList(Resource):
 class Pin(Resource):
     """Domain object for Michigan Maps pin. More docs to go here."""
 
-    @marshal_with_field(pin_response_schema)
+    @marshal_with_schema(pin_response_schema)
     def get(self, id):
         return pin_dao.get_item(id)
 
-    @marshal_with_field(pin_response_schema)
+    @marshal_with_schema(pin_response_schema)
     def delete(self, id):
         return pin_dao.delete_item(id)
 
-    @marshal_with_field(pin_response_schema)
+    @marshal_with_schema(pin_response_schema)
     def patch(self, id):
         new_fields = request.get_json(force=True)
         return pin_dao.update_item(id, new_fields)
